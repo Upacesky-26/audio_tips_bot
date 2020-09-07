a406628ced07dd3d94a0ab35f8357be869131a82
Upacesky-26 <baohm26@gmail.com>
2020-09-07 12:35:38 +0200

    add messages Hash

--- main.rb
+++ main.rb
@@ -1,5 +1,17 @@
 require 'elephrame'
-AudioTipsBot = Elephrame::Bots::Periodic.new '60s'
+require 'yaml'
+
+messages = {
+  welcome
+}
+
+AudioTipsBot = Elephrame::Bots::PeriodicInteract.new '60s'
+
+AudioTipsBot.on_reply do |bot, post|
+  user_name = post.account.display_name
+  bot.reply("Hello #{user_name}, I'm a bot specialized in giving advices about all things music production and audio. \/n")
+
+end
 
 AudioTipsBot.run do |bot|
   bot.post("This is a test")
